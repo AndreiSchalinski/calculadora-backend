f7eb4c8f98a0ab670cfbfe7cb22757c867948374
AndreiSchalinski <andreischalinski@hotmail.com>
2025-10-28 21:43:07 -0300

    Implementação endpoint calcular form calculadora

--- src/main/java/com/desafio/calculadora/controller/ControllerCalculadora.java
+++ src/main/java/com/desafio/calculadora/controller/ControllerCalculadora.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.desafio.calculadora.dto.JurosDTO;
+import com.desafio.calculadora.dto.FormDTO;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/api/calculadora")
 public class ControllerCalculadora {
 
-
-    @PostMapping("/calcular")
-    public ResponseEntity<?> getString(@RequestBody JurosDTO dto){
+    @PostMapping("calcular")
+    public ResponseEntity<FormDTO> controllerCalcularEmprestimo(@RequestBody FormDTO dto){
         return ResponseEntity.ok().body(dto);
     }
     
